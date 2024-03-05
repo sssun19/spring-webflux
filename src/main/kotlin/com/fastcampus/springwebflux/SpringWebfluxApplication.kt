524b6ce1a66c9e59b073b975673e834c2d2613af
sssun <98_0509@naver.com>
2024-03-05 22:19:16 +0900

    firstCommit

---
+++ src/main/kotlin/com/fastcampus/springwebflux/SpringWebfluxApplication.kt
@@ -0,0 +1,11 @@
+package com.fastcampus.springwebflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringWebfluxApplication>(*args)
+}
