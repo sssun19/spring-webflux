383125939f7f5e32f1f5c78c57ed19687efff504
sssun <98_0509@naver.com>
2024-03-06 21:09:24 +0900

    UserRouter

--- src/main/kotlin/com/fastcampus/springwebflux/fn/Router.kt
+++ src/main/kotlin/com/fastcampus/springwebflux/fn/Router.kt
@@ -8,12 +8,27 @@ import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
 
 @Configuration
+// 클라이언트로부터 전달 받은 요청을 해석하고 그에 맞는 핸들러로 전달하는 역할
+// configuration annotation 으로 설정해주어야 함 Bean 으로 라우터 함수 등록해주어야 함
 class Router {
 
     @Bean
     fun helloRouter(handler: HelloHandler) : RouterFunction<ServerResponse> =
         route()
             .GET("/", handler::sayHello)
+//            .POST("/", handler::sayHello)
             .build()
 
+    @Bean
+    fun userRouter(handler: UserHandler) : RouterFunction<ServerResponse> =
+//        route() // route 함수를 사용하면 http get post push delete 함수 사용 가능
+//            .GET("/users/{id}", handler::getUser)
+//            .GET("/users", handler::getAll)
+//            .build()
+        router {
+            "/users".nest {
+                GET("/{id}", handler::getUser)
+                GET("", handler::getAll)
+            }
+        }
 }
--- src/main/kotlin/com/fastcampus/springwebflux/fn/UserHandler.kt
+++ src/main/kotlin/com/fastcampus/springwebflux/fn/UserHandler.kt
@@ -6,7 +6,7 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
 
-class User(val id: Long, val email: String)
+data class User(val id: Long, val email: String)
 
 @Component
 class UserHandler {
@@ -14,15 +14,19 @@ class UserHandler {
     val users = listOf(
         User(id = 1, email = "user1@gmail.com"),
         User(id = 2, email = "user2@gmail.com")
+
     )
 
-    fun getUser(request: ServerRequest): Mono<ServerResponse> =
-        users.find { request.pathVariable("id").toLong() == it.id }
+    fun getUser(req: ServerRequest) : Mono<ServerResponse> =
+        users.find { req.pathVariable("id").toLong() == it.id }
             ?.let {
                 ServerResponse.ok().bodyValue(it)
-            } ?: ServerResponse.notFound().build()
+            }?: ServerResponse.notFound().build()
 
-    fun getAll(request: ServerRequest): Mono<ServerResponse> =
+
+    fun getAll(req: ServerRequest) : Mono<ServerResponse> =
         ServerResponse.ok().bodyValue(users)
+
+
 
 }
